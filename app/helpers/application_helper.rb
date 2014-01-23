f3cf3dc972f5dc688a809c37ee632b42f3f54947
Chun Hian Bong <dropbox@hotmail.my>
2014-01-23 15:11:14 -0800

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "RRT"
+    base_title = "Andy Chao"
     if page_title.empty?
       base_title
     else
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
   describe "Home page" do
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("RRT")
-    end
- 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end 
+    before { visit root_path }
+
+    it { should have_content('real estate') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
   end
 
   describe "About page" do
-
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
     end
   end
 end
