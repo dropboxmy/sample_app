ec05736302d9fc29b959e837aee1bc3633a8458a
Chun Hian Bong <dropbox@hotmail.my>
2014-03-18 22:32:51 -0700

    Make a basic User model (including secure passwords)

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe User do
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  # format validation for email address
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_bazlcom foo@bar+baz.com]
+      addresses.each do | invalid_address |
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[email@dd.ress mj@nb.com]
+      addresses.each do | valid_address |
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+end
